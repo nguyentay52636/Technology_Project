8589441c0248301d888280f123e14c2462bd136d
tay52636 <phuongtay52636@gmail.com>
2026-03-28 00:02:29 +0700

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 
 // 1. Import các component của bạn vào đây
 import Navbar from "@/components/Navbar"; 
 import Footer from "@/components/Footer";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +32,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
       {/* 2. Thêm flex-col để Footer luôn nằm dưới cùng */}
       <body className="min-h-full flex flex-col bg-white dark:bg-black">
